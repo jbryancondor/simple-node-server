8745876363c6c41d032e8c965af872b8074fce52
Bryan Condor <bcondor@addi.com>
2024-08-27 15:51:47 -0500

    add /pvt/orders

--- main.js
+++ main.js
@@ -38,6 +38,117 @@ app.put('/webhook', (req, res) => {
     res.status(200).end();
 })
 
+app.post('/pvt/orders', (req, res) => {
+    console.log('POST /pvt/orders');
+    console.log('POST body:', req.body);
+
+    res.json({
+        "isTest": true,
+        "marketplaceOrderId": "1457570502001-01",
+        "orderId": "7890",
+        "followUpEmail": "srivas@addi.com",
+        "items": [
+            {
+                "id": "REFID002",
+                "quantity": 1,
+                "seller": "1",
+                "commission": 1500,
+                "freightCommission": 1000,
+                "price": 67200,
+                "bundleItems": [],
+                "itemAttachment": {
+                    "name": null,
+                    "content": {}
+                },
+                "attachments": [],
+                "priceTags": [],
+                "measurementUnit": "un",
+                "unitMultiplier": 1,
+                "isGift": false
+            }
+        ],
+        "clientProfileData": {
+            "email": "293b18ed65644179ad712147b23a783a@ct.vtex.com.br",
+            "firstName": "Sergio",
+            "lastName": "Rivas",
+            "documentType": "cedulaCOL",
+            "document": "30012312",
+            "phone": "+573001231234",
+            "corporateName": null,
+            "tradeName": null,
+            "corporateDocument": null,
+            "stateInscription": null,
+            "corporatePhone": null,
+            "isCorporate": false,
+            "userProfileId": null
+        },
+        "shippingData": {
+            "isFOB": false,
+            "address": {
+                "addressType": "pickup",
+                "receiverName": "Juliana",
+                "addressId": "548304ed-dd40-4416-b12b-4b32bfa7b1e0",
+                "postalCode": "110110",
+                "city": "Bogotá DC",
+                "state": "Bogotá",
+                "country": "COL",
+                "street": "Calle 93",
+                "number": "18",
+                "neighborhood": "El chicó",
+                "complement": "Loja 10",
+                "reference": "Next to the unicorn statue",
+                "geoCoordinates": [
+                    4.678248,
+                    -74.054796
+                ]
+            },
+            "selectedAddresses": [
+                {
+                    "addressType": "pickup",
+                    "receiverName": "Juliana",
+                    "addressId": "548304ed-dd40-4416-b12b-4b32bfa7b1e0",
+                    "postalCode": "19256",
+                    "city": "Bogotá DC",
+                    "state": "Bogotá",
+                    "country": "COL",
+                    "street": "Calle 93",
+                    "number": "18",
+                    "neighborhood": "El chicó",
+                    "complement": "Loja 10",
+                    "reference": "Next to the unicorn statue",
+                    "geoCoordinates": [
+                        4.678248,
+                        -74.054796
+                    ]
+                }
+            ],
+            "logisticsInfo": [
+                {
+                    "itemIndex": 0,
+                    "selectedSla": "Curbside pickup",
+                    "addressId": "548304ed-dd40-4416-b12b-4b32bfa7b1e0",
+                    "selectedDeliveryChannel": "pickup-in-point",
+                    "deliveryIds": [],
+                    "lockTTL": "10d",
+                    "shippingEstimate": "0bd",
+                    "price": 0,
+                    "deliveryWindow": {
+                        "startDateUtc": "2024-02-08T08:00:00+00:00",
+                        "endDateUtc": "2024-02-10T13:00:00+00:00",
+                        "price": 0,
+                        "listPrice": 0
+                    }
+                }
+            ],
+            "trackingHints": [],
+            "contactInformation": []
+        },
+        "customData": null,
+        "paymentData": null,
+        "allowMultipleDeliveries": true
+    }).status(200).end();
+})
+
 app.listen(port, () => {
     console.log(`Simple Server app listening on port ${port}`)
 })
