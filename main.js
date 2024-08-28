165f702e4bf72a3d70853f0b32208621ca71715b
Sergio Rivas <srivas@addi.com>
2024-08-28 18:41:11 -0500

    update simulation

--- main.js
+++ main.js
@@ -91,73 +91,93 @@ app.post('/api/pvt/orders', (req, res) => {
 app.post('/pvt/orderForms/simulation', (req, res) => {
     const body = req.body;
 
-    res.json(
-        {
-            "country": "COL",
-            "items": body.items,
-            "logisticsInfo": [
-                {
-                    "itemIndex": 0,
-                    "quantity": 6,
-                    "shipsTo": [
-                        "COL"
-                    ],
-                    "slas": [
-                        {
-                            "id": "Curbside pickup",
-                            "deliveryChannel": "pickup-in-point",
-                            "name": "Curbside pickup",
-                            "shippingEstimate": "0bd",
-                            "price": 0,
-                            "availableDeliveryWindows": [
-                                {
-                                    "startDateUtc": "2024-02-08T08:00:00+00:00",
-                                    "endDateUtc": "2024-02-10T13:00:00+00:00",
-                                    "price": 0
-                                }
-                            ],
-                            "pickupStoreInfo": {
-                                "isPickupStore": true,
-                                "friendlyName": "Santa Felicidade",
-                                "address": {
-                                    "addressType": "pickup",
-                                    "receiverName": "Juliana",
-                                    "addressId": "548304ed-dd40-4416-b12b-4b32bfa7b1e0",
-                                    "postalCode": "110110",
-                                    "city": "Bogotá DC",
-                                    "state": "Bogotá",
-                                    "country": "COL",
-                                    "street": "Calle 93",
-                                    "number": "18",
-                                    "neighborhood": "El chicó",
-                                    "complement": "Loja 10",
-                                    "reference": "Next to the unicorn statue",
-                                    "geoCoordinates": [
-                                        4.678248,
-                                        -74.054796
-                                    ]
-                                },
-                                "additionalInfo": ""
-                            }
-                        }
-                    ],
-                    "stockBalance": 199,
-                    "deliveryChannels": [
-                        {
-                            "id": "delivery",
-                            "stockBalance": 179
-                        },
-                        {
-                            "id": "pickup-in-point",
-                            "stockBalance": 20
-                        }
-                    ]
-                }
+    res.json({
+        "country": "COL",
+        "items": [
+          {
+            "id": "REFID002",
+            "listPrice": 67203,
+            "measurementUnit": "un",
+            "merchantName": null,
+            "offerings": [
+              {
+                "type": "Warranty",
+                "id": "5",
+                "name": "1 year warranty",
+                "price": 10000
+              }
             ],
-            "postalCode": "80250000",
-            "allowMultipleDeliveries": true
-        }
-    ).status(200).end();
+            "price": 67203,
+            "priceTags": [],
+            "priceValidUntil": "2024-10-01T22:58:28.143",
+            "quantity": 1,
+            "requestIndex": 0,
+            "seller": body.items[0].seller,
+            "unitMultiplier": 1
+          }
+        ],
+        "logisticsInfo": [
+          {
+            "itemIndex": 0,
+            "quantity": 6,
+            "shipsTo": [
+              "COL"
+            ],
+            "slas": [
+              {
+                "id": "Curbside pickup",
+                "deliveryChannel": "pickup-in-point",
+                "name": "Curbside pickup",
+                "shippingEstimate": "0bd",
+                "price": 0,
+                "availableDeliveryWindows": [
+                  {
+                    "startDateUtc": "2024-02-08T08:00:00+00:00",
+                    "endDateUtc": "2024-02-10T13:00:00+00:00",
+                    "price": 0
+                  }
+                ],
+                "pickupStoreInfo": {
+                  "isPickupStore": true,
+                  "friendlyName": "Santa Felicidade",
+                  "address": {
+                "addressType": "pickup",
+                "receiverName": "Juliana",
+                "addressId": "548304ed-dd40-4416-b12b-4b32bfa7b1e0",
+                "postalCode": "110110",
+                "city": "Bogotá DC",
+                "state": "Bogotá",
+                "country": "COL",
+                "street": "Calle 93",
+                "number": "18",
+                "neighborhood": "El chicó",
+                "complement": "Loja 10",
+                "reference": "Next to the unicorn statue",
+                "geoCoordinates": [
+                    4.678248,
+                    -74.054796
+                ]
+            },
+                  "additionalInfo": ""
+                }
+              }
+            ],
+            "stockBalance": 199,
+            "deliveryChannels": [
+              {
+                "id": "delivery",
+                "stockBalance": 179
+              },
+              {
+                "id": "pickup-in-point",
+                "stockBalance": 20
+              }
+            ]
+          }
+        ],
+        "postalCode": "80250000",
+        "allowMultipleDeliveries": true
+      }).status(200).end();
 })
 
 app.post('/pvt/orders', (req, res) => {
